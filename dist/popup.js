59fda36e9315facf8bace3f21cbb8c5a363ac0f4
Dan Chadwick <dan.chadwick@rareloop.com>
2024-01-01 11:27:32 +0000

    Tweak

--- dist/popup.js
+++ dist/popup.js
@@ -26,8 +26,8 @@ var Popup = /** @class */ (function (_super) {
         if (title === void 0) { title = ''; }
         if (rectangle === void 0) { rectangle = null; }
         var _a, _b, _c;
-        var popupWidth = 0.4 * canvas.width;
-        var popupHeight = 0.4 * canvas.height;
+        var popupWidth = 0.8 * canvas.width;
+        var popupHeight = 0.8 * canvas.height;
         _this = _super.call(this, scene, name, rectangle !== null && rectangle !== void 0 ? rectangle : new rectangle_1.Rectangle((canvas.width - popupWidth) / 8, (canvas.height - popupHeight) / 8, popupWidth, popupHeight)) || this;
         _this.canvas = canvas;
         _this.title = new textItem_1.TextItem(scene, "".concat(name, ".title"), { x: _this.rectangle.x + 40, y: _this.rectangle.y + window.gamework.constants.POPUP.padding }, window.gamework.constants.POPUP.fontSize, window.gamework.constants.POPUP.fontType, window.gamework.constants.POPUP.textColour, title);
--- src/popup.js
+++ src/popup.js
@@ -4,8 +4,8 @@ import { TextItem } from "./textItem";
 
 export class Popup extends GameObject {
     constructor(name, scene, canvas, title = '', rectangle = null) {
-        const popupWidth = 0.4 * canvas.width;
-        const popupHeight = 0.4 * canvas.height;
+        const popupWidth = 0.8 * canvas.width;
+        const popupHeight = 0.8 * canvas.height;
         
         super(scene, name, rectangle ?? new Rectangle((canvas.width - popupWidth) / 8, (canvas.height - popupHeight) / 8, popupWidth, popupHeight));
         this.canvas = canvas;
